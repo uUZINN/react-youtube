e5dc49b09ce88ffc6993d63ef34f845d0e99ee56
uzin4916 <89904583+uUZINN@users.noreply.github.com>
2023-10-17 14:21:13 +0900

    페이지 만들기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Today from './pages/Today'
+import Youtuber from './pages/Youtuber'
+import Not from './pages/Not'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/today' element={<Today />} />
+                <Route path='/youtuber' element={<Youtuber />} />
+                <Route path='*' element={<Not />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
